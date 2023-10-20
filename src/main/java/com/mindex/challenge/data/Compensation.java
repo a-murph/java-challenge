959e1d1cef15fca2c77fb04efa823e23b7b709bb
Andrew Murphy <andrew.murphy@conduent.com>
2023-10-20 16:07:54 -0400

    Add Compensation type and create/read endpoints

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,38 @@
+package com.mindex.challenge.data;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Compensation {
+  private String employeeId;
+  private float salary;
+  private @DateTimeFormat(pattern = "yyyy-MM-dd") Date effectiveDate;
+
+  public Compensation() {
+  }
+
+  public String getEmployeeId() {
+    return employeeId;
+  }
+
+  public void setEmployeeId(String employeeId) {
+    this.employeeId = employeeId;
+  }
+
+  public float getSalary() {
+    return salary;
+  }
+
+  public void setSalary(float salary) {
+    this.salary = salary;
+  }
+
+  public Date getEffectiveDate() {
+    return effectiveDate;
+  }
+
+  public void setEffectiveDate(Date effectiveDate) {
+    this.effectiveDate = effectiveDate;
+  }
+}
---
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -0,0 +1,54 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.service.CompensationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CompensationServiceImpl implements CompensationService {
+
+  private static final Logger LOG = LoggerFactory.getLogger(CompensationServiceImpl.class);
+
+  @Autowired
+  private CompensationRepository compensationRepository;
+  @Autowired
+  private EmployeeRepository employeeRepository;
+
+  @Override
+  public Compensation create(Compensation compensation) {
+    LOG.debug("Creating compensation for employee [{}]", compensation.getEmployeeId());
+
+    Compensation existingComp = compensationRepository.findByEmployeeId(compensation.getEmployeeId());
+    if (existingComp != null) {
+      throw new RuntimeException("Compensation already exists");
+    }
+
+    Employee employee = employeeRepository.findByEmployeeId(compensation.getEmployeeId());
+    if (employee == null) {
+      throw new RuntimeException("Invalid employeeId: " + compensation.getEmployeeId());
+    }
+
+    compensationRepository.insert(compensation);
+
+    return compensation;
+  }
+
+  @Override
+  public Compensation read(String id) {
+    LOG.debug("Finding compensation with id [{}]", id);
+
+    Compensation compensation = compensationRepository.findByEmployeeId(id);
+
+    if (compensation == null) {
+      throw new RuntimeException("Invalid employeeId: " + id);
+    }
+
+    return compensation;
+  }
+}
