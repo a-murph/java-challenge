9caf9a8aca3d3276229f43a2a5682b353ea5ffe1
Andrew Murphy <andrew.murphy@conduent.com>
2023-10-20 14:33:58 -0400

    Add ReportingStructure type and endpoint

--- src/main/java/com/mindex/challenge/dao/EmployeeRepository.java
+++ src/main/java/com/mindex/challenge/dao/EmployeeRepository.java
@@ -1,10 +1,41 @@
 package com.mindex.challenge.dao;
 
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 @Repository
+@Document("employees")
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
     Employee findByEmployeeId(String employeeId);
+
+    @Query(value = "{ employeeId: { $in: ?0 }, directReports: { $exists: true, $ne: [] } }")
+    List<Employee> findAllByEmployeeIdsAndDirectReportsNotEmpty(List<String> employeeIds);
+
+    // Aggregation to recursively find all employees in the direct report tree under the given employee
+    // Works in Mongo Shell but the fake local implementation created by mongo-java-server does not support $graphLookup
+    @Aggregation(pipeline = {"{$match: {\n" +
+            "   employeeId: ?0\n" +
+            "}}",
+            "{$graphLookup: {\n" +
+            "   from: 'employees',\n" +
+            "   startWith: '$directReports.employeeId',\n" +
+            "   connectFromField: 'directReports.employeeId',\n" +
+            "   connectToField: 'employeeId',\n" +
+            "   as: 'allReports'\n" +
+            "}}",
+            "{$project: {\n" +
+            "   employeeId: '$employeeId',\n" +
+            "   numberOfReports: { $size: '$allReports' }\n" +
+            "}}"})
+    AggregationResults<ReportingStructure> getAllDirectReportsByEmployeeId(String employeeId);
 }
--- src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/EmployeeServiceImpl.java
@@ -2,12 +2,18 @@ package com.mindex.challenge.service.impl;
 
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.EmployeeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -29,7 +35,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee read(String id) {
-        LOG.debug("Creating employee with id [{}]", id);
+        LOG.debug("Finding employee with id [{}]", id);
 
         Employee employee = employeeRepository.findByEmployeeId(id);
 
@@ -45,5 +51,50 @@ public class EmployeeServiceImpl implements EmployeeService {
         LOG.debug("Updating employee [{}]", employee);
 
         return employeeRepository.save(employee);
+    }
+
+    @Override
+    public ReportingStructure getReportingStructure(String id) {
+        Employee employee = employeeRepository.findByEmployeeId(id);
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        ReportingStructure reporting = new ReportingStructure();
+        
+        // // More efficient recursive method of getting all reports - doesn't work with local mongo-java-server plugin
+        // ReportingStructure aggResults = employeeRepository.getAllDirectReportsByEmployeeId(id).getMappedResults().get(0);
+        // reporting.setEmployeeId(aggResults.getEmployeeId());
+        // reporting.setNumberOfReports(aggResults.getNumberOfReports());
+
+        // Get IDs of each employee in the directReports array for the selected parent employee (ID is the only field present on these employee objects)
+        List<String> allDirectReportIds = new ArrayList<String>(employee.getDirectReports().stream().map(e -> e.getEmployeeId()).collect(Collectors.toList()));
+        List<String> employeeIdsToCheckNesting = new ArrayList<String>(allDirectReportIds);
+
+        // As long as there are more employeeIds that haven't been checked for nesting, keep traversing through levels
+        while (!employeeIdsToCheckNesting.isEmpty()) {
+            // Get full employee object for each ID that needs to be checked for nesting
+            // Only get the full object if that employee actually has a populated directReports array - if there is no more nesting, this will return empty
+            List<Employee> employeesWithNesting = employeeRepository.findAllByEmployeeIdsAndDirectReportsNotEmpty(employeeIdsToCheckNesting);
+
+            // Empty this list to refill with the next layer of nesting (if needed)
+            employeeIdsToCheckNesting.clear();
+
+            // If more nesting was found, add the newly-found nested employee IDs to the full list
+            // Also add them to the "checkNesting" array so that this process can be repeated to check for another layer of nesting
+            if (!employeesWithNesting.isEmpty()) {
+                List<String> nextLevelEmployeeIds = new ArrayList<String>();
+                employeesWithNesting.forEach(empl -> {
+                    empl.getDirectReports().forEach(dr -> nextLevelEmployeeIds.add(dr.getEmployeeId()));
+                });
+                allDirectReportIds.addAll(nextLevelEmployeeIds);
+                employeeIdsToCheckNesting.addAll(nextLevelEmployeeIds);
+            }
+        }
+
+        reporting.setEmployeeId(employee.getEmployeeId());
+        reporting.setNumberOfReports(allDirectReportIds.size());
+
+        return reporting;
     }
 }
